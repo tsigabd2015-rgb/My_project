f26b33e99383b45e099535501d34ce0f4bf29504
tsigabd2015-rgb <tsigabd2015@gmail.com>
2026-07-23 20:37:07 +1200

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = {
+    "Month": ["Jan", "Feb", "Mar", "Apr"],
+    "Sales": [10, 25, 18, 30]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+plt.bar(df["Month"], df["Sales"])
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.title("Sales by Month")
+plt.show()
+
+plt.hist(df["Sales"], bins=5)
+plt.xlabel("Sales")
+plt.ylabel("Frequency")
+plt.title("Distribution of Sales")
+plt.show()
